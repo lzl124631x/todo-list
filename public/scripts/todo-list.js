efba3e0e5119d55e35e2c296097c82c52b17799e
Richard LIU <liuzhenglaichn@gmai.com>
2016-09-10 14:29:53 +0800

    added post method

--- public/scripts/todo-list.js
+++ public/scripts/todo-list.js
@@ -34,6 +34,47 @@ var TodoList = React.createClass({
   }
 });
 
+var TodoForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onTodoSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
+  },
+  render: function() {
+    return (
+      <form className="todoForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
 var TodoBox = React.createClass({
   loadTodosFromServer: function() {
     $.ajax({
@@ -82,6 +123,7 @@ var TodoBox = React.createClass({
       <div className="todoBox">
         <h1>Todos</h1>
         <TodoList data={this.state.data} />
+        <TodoForm onTodoSubmit={this.handleTodoSubmit} />
       </div>
     );
   }
