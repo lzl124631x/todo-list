6d1577122a36cd07cbcbc5b6d54808e3a52cd70c
Richard LIU <liuzhenglaichn@gmail.com>
2016-09-10 14:29:53 +0800

    added post method

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var TODO_FILE = path.join(__dirname, 'todos.json');
 
@@ -14,6 +17,32 @@ app.get('/api/todos', function(req, res) {
       process.exit(1);
     }
     res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/todos', function(req, res) {
+  fs.readFile(TODO_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var todos = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    var newTodo = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text,
+    };
+    todos.push(newTodo);
+    fs.writeFile(TODO_FILE, JSON.stringify(todos, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(todos);
+    });
   });
 });
 
