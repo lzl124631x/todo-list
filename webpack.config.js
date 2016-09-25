d7c76fda4eeb917f977fb48c62f7d4827a338874
Richard Liu <liuzhenglaichn@gmail.com>
2016-09-25 20:19:02 +0800

    introduced webpack.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, 'public/scripts'),
+  entry: './todo-list.js',
+  output: {
+    path: path.join(__dirname, '/public/scripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+    {
+      test: /\.js|jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }
+    ]
+  }
+}
