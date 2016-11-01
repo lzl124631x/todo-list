18c34c2d9a3c7fb3b4c108f8497f2ace566fa31a
Richard LIU <liuzhenglaichn@gmail.com>
2016-11-01 22:39:30 +0800

    introduced redux-logger; migrating all user interaction handlers to todo-list (one source of truth) NOT FINISHED.

--- public/scripts/components/todo-list.js
+++ public/scripts/components/todo-list.js
@@ -7,17 +7,22 @@ import AddTodo from '../containers/add-todo'
 import { Motion, spring } from 'react-motion'
 import { ITEM_HEIGHT } from '../containers/util'
 
+const H_PAN_THRESHOLD = 50
+
 const DEFAULT = 'DEFAULT'
 const PULL_DOWN_LIST = 'PULL_DOWN_LIST'
 const RELEASED_AND_ADD = 'RELEASED_AND_ADD'
 const CANCEL_ADD = 'CANCEL_ADD'
+const PULL_RIGHT_ITEM = 'PULL_RIGHT_ITEM'
+const PULL_LEFT_ITEM = 'PULL_LEFT_ITEM'
+const ITEM_TOGGLED = 'ITEM_TOGGLED'
 
 const INITIAL_STATE = {
   press: [0, 0],
   delta: [0, 0],
   isPressed: false,
-  listOperationDisabled: false,
-  uiState: DEFAULT
+  pressedItem: null,
+  uiState: DEFAULT,
 }
 
 class TodoList extends React.Component {
@@ -29,7 +34,6 @@ class TodoList extends React.Component {
     this.handleMouseMove = this.handleMouseMove.bind(this)
     this.handleTouchEnd = this.handleTouchEnd.bind(this)
     this.handleMouseUp = this.handleMouseUp.bind(this)
-    this.handleListOperationDisabled = this.handleListOperationDisabled.bind(this)
     this.cancelAdd = this.cancelAdd.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
     this.onListRest = this.onListRest.bind(this)
@@ -41,7 +45,11 @@ class TodoList extends React.Component {
   }
 
   handleMouseDown ({ pageX, pageY }) {
-    this.setState({ press: [ pageX, pageY ], isPressed: true })
+    this.setState({
+      press: [ pageX, pageY ],
+      isPressed: true,
+      pressedItem: this.props.todos.find(t => t.order == Math.floor(pageY / ITEM_HEIGHT))
+    })
   }
   
   handleTouchMove (e) {
@@ -53,8 +61,16 @@ class TodoList extends React.Component {
     if (this.state.isPressed) {
       const [x, y] = this.state.press
       const delta = [ pageX - x, pageY - y ]
-      this.setState({ delta: delta})
-      if (delta[1] > 0) {
+      this.setState({ delta: delta })
+      const { uiState } = this.state
+      if (uiState !== DEFAULT) {
+        if (uiState === PULL_RIGHT_ITEM && Math.abs(delta[0]) > H_PAN_THRESHOLD) {
+          this.setState({ uiState: ITEM_TOGGLED })
+        }
+      } else if (Math.abs(delta[0]) > Math.abs(delta[1])) {
+        this.setState({ uiState: delta[0] > 0 ? PULL_RIGHT_ITEM : PULL_LEFT_ITEM })
+      } else if (delta[1] > 0) {
+        // pull down
         this.setState({ uiState: PULL_DOWN_LIST })
       }
     }
@@ -72,14 +88,15 @@ class TodoList extends React.Component {
     let state = Object.assign({}, INITIAL_STATE)
     if (this.state.delta[1] > ITEM_HEIGHT) {
       state.uiState = RELEASED_AND_ADD
-    } else {
+    } else if (this.state.delta[1] > 0){
       state.uiState = CANCEL_ADD
     }
+    console.log('reseting ', this.state, state)
     this.setState(state)
   }
 
-  handleListOperationDisabled () {
-    this.setState(Object.assign({}, INITIAL_STATE, { listOperationDisabled: true }))
+  componentWillUpdate(nextProps, nextState) {
+console.log('list',nextState)
   }
 
   cancelAdd (e) {
@@ -104,8 +121,8 @@ class TodoList extends React.Component {
       y = (y - 5 * ITEM_HEIGHT) / 5 + 5 * ITEM_HEIGHT
     }
     let enableMotion = false
-    let disableItemOperation = true
-    let style = { y: this.state.isPressed && !this.state.isItemLongPressed ? y : 0, backdropOpacity: spring(0) }
+    let disableItemOperation = (uiState !== DEFAULT)
+    let style = { x: 0, y: this.state.isPressed && !this.state.isItemLongPressed ? y : 0, backdropOpacity: spring(0) }
     let releaseAndAdd = (uiState === RELEASED_AND_ADD)
     let cancelAdd = (uiState == CANCEL_ADD)
     if (uiState === RELEASED_AND_ADD) {
@@ -115,6 +132,8 @@ class TodoList extends React.Component {
 
     } else if (uiState === CANCEL_ADD) {
       style.y = spring(0)
+    } else if (uiState === PULL_RIGHT_ITEM || uiState === PULL_LEFT_ITEM) {
+      style.y = 0
     }
     return (
       <Motion style={style} onRest={this.onListRest}>
@@ -129,20 +148,35 @@ class TodoList extends React.Component {
               onTouchEnd={this.handleTouchEnd}
               onMouseUp={this.handleMouseUp}>
               {
-                todos.map((todo, i) =>
-                  <Todo
+                todos.map((todo, i) => {
+                  let { delta, pressedItem } = this.state
+                  const isPressed = pressedItem && pressedItem.id === todo.id
+                  let x
+                  if (!isPressed) {
+                    x = 0
+                  } else {
+                    x = delta[0]
+                    if ((uiState === PULL_RIGHT_ITEM && x < 0)
+                      || (uiState === PULL_LEFT_ITEM && x > 0)) {
+                      x = 0
+                    }
+                    console.log('pressed!', uiState, delta[0], x)
+                  }
+                  return <Todo
                     key={todo.id}
                     {...todo}
                     onToggle={() => onToggle(todo.id)}
                     onDelete={() => onDelete(todo.id)}
                     onReorder={(to) => onReorder(todo.id, to)}
                     itemCount={todos.length}
+                    x={x}
                     y={y}
+                    isPressed={isPressed}
                     zIndex={ uiState == RELEASED_AND_ADD ? 0 : uiState == CANCEL_ADD ? 0 : (todo.order === 0 ? 10000: 0)}
                     enableMotion={enableMotion}
                     disableItemOperation={disableItemOperation}
-                    onListOperationDisabled={this.handleListOperationDisabled}
-                  />)
+                    uiState={uiState}
+                  />})
               }
               <div className="backdrop" style={{
                 display: backdropOpacity === 0 ? "none" : "block",
